44ea485eb335586c5216895c3f507954fd4efca9
mmtq <mirtarhimul@gmail.com>
2025-02-24 18:41:51 +0600

    Real time chat with django channels and websocket

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -10,7 +10,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+import a_rtchat.routing 
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                a_rtchat.routing.websocket_urlpatterns
+            )
+        )
+    )
+})
