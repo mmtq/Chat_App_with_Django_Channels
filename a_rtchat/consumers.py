92315354c358d6f0e58f387ae3fe65d92f7f7ce3
mmtq <mirtarhimul@gmail.com>
2025-03-02 23:23:28 +0600

    online tracker with websocket

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -15,6 +15,10 @@ class ChatroomConsumer(WebsocketConsumer):
             self.channel_name
         )
         
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+        
         self.accept()
         
     def disconnect(self, close_code):
@@ -22,7 +26,11 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name,
             self.channel_name
         )
-    
+        
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         body = text_data_json['body']
@@ -38,11 +46,30 @@ class ChatroomConsumer(WebsocketConsumer):
         )
             
     def message_handler(self, event):
+        print("message_handler")
         message_id = event['message_id']
         message = GroupMessages.objects.get(id=message_id)
         context = {
             'message': message ,
             'user': self.user
             }
-        html = render_to_string('a_rtchat/partials/chat_message.html', context)
+        html = render_to_string('a_rtchat/partials/chat_message_p.html', context)
+        self.send(text_data=html)
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count()-1
+        
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name,
+            event
+        )
+        
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        html = render_to_string('a_rtchat/partials/online_count.html', {'online_count': online_count})
         self.send(text_data=html)
--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True)
-    
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
     def __str__(self):
         return self.group_name
 
@@ -18,4 +18,4 @@ class GroupMessages(models.Model):
         return f'{self.author}: {self.body}'
     
     class Meta:
-        ordering = ['created']
+        ordering = ['-created']
